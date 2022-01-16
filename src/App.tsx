d17a65656c91ffb8fa5e89bcf20e0dcc97d99da2
p118722 <p118722@ap.be>
2022-01-16 12:26:51 +0100

    Add statistics

--- src/App.tsx
+++ src/App.tsx
@@ -116,6 +116,8 @@ const Modal = () => {
   const dispatch = useDispatch();
 
   let currentGame = useSelector((state: RootState) => state.gameState.currentGame);
+  let statistics = useSelector((state: RootState) => state.gameState.statistics);
+
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -168,6 +170,30 @@ ${board}
             <button onClick={() => { setHidden((hide) => { return !hide }) }} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="default-modal">
               <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
             </button>
+          </div>
+
+          <div className="flex flex-col items-stretch">
+            <div className="flex justify-center">
+              <h1 className="text-sm font-semibold uppercase text-gray-900">Statistieken</h1>
+            </div>
+            <div className="flex flex-row gap-1 p-2">
+              <div className="flex-1">
+                <div className="flex justify-center items-center text-xl font-bold">{statistics.gamesPlayed}</div>
+                <div className="flex justify-center items-center text-xs">Played</div>
+              </div>
+              <div className="flex-1">
+                <div className="flex justify-center items-center text-xl font-bold">{Math.floor((statistics.gamesWon/statistics.gamesPlayed)*100)}%</div>
+                <div className="flex justify-center items-center text-xs">Win</div>
+              </div>
+              <div className="flex-1">
+                <div className="flex justify-center items-center text-xl font-bold">{statistics.currentStreak}</div>
+                <div className="flex justify-center items-center text-xs">Current Streak</div>
+              </div>
+              <div className="flex-1">
+                <div className="flex justify-center items-center text-xl font-bold">{statistics.maxStreak}</div>
+                <div className="flex justify-center items-center text-xs">Max Streak</div>
+              </div>
+            </div>
           </div>
 
 
